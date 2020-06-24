05ba8d6bd745814c1d1fb35482df84cc64dc9f65
Arthur Palhano <pauloart.hur670@gmail.com>
2020-06-23 23:36:27 -0300

    Upgrade

--- index.py
+++ index.py
@@ -1,49 +1,66 @@
 from prettytable import PrettyTable
-import math, os, json
+import math
+import os
+import json
+
+field_names = ["Estrela", "Tipo", "Temp. (K)", f"Lum. (○)", f"Massa (M○)", "Comp. de Onda (μm)",
+               f"Raio (R○)", "Mag. Aparente", "Mag. Absoluta", "Dist (LY)", "K"]
+
+sortField = "Estrela"
+
 
 def createTable():
     table = PrettyTable()
-    table.field_names = ["Estrela", "Tipo", "Temp. (K)", f"Lum. (○)", f"Massa (M○)", "Comp. de Onda (μm)", f"Raio (R○)", "Mag. Aparente", "Mag. Absoluta", "Dist (LY)", "K"]
-    table.sortby = "Estrela"
+    table.field_names = field_names
+    table.sortby = sortField
     return table
+
 
 def loginStar():
     star = []
-    star.append(str(input("Nome da Estrela: ")))#0
-    star.append(str(input("Tipo: ")))#1
-    star.append(float(input("Temperatura em Kelvin: ")))#2
-    star.append(float(input("Luminosidade em ○: ")))#3
-    star.append(float(input("Massa em ○: ")))#4
-    star.append(float(input("Comprimento de Onda em μm: ")))#5
-    star.append(float(input("Raio em ○: ")))#6
-    star.append(float(input("Magnitude Aparente: ")))#7
-    star.append(float(input("Magnitude Absoluta: ")))#8
-    star.append(float(input("Distância em LY: ")))#9
+    star.append(str(input("Nome da Estrela: ")))  # 0
+    star.append(str(input("Tipo: ")))  # 1
+    star.append(float(input("Temperatura em Kelvin: ")))  # 2
+    star.append(float(input("Luminosidade em ○: ")))  # 3
+    star.append(float(input("Massa em ○: ")))  # 4
+    star.append(round(waveLength(star[2]), 2))  # 5
+    star.append(float(input("Raio em ○: ")))  # 6
+    star.append(float(input("Magnitude Aparente: ")))  # 7
+    star.append(float(input("Magnitude Absoluta: ")))  # 8
+    star.append(float(input("Distância em LY: ")))  # 9
 
     os.system("cls")
-    
+
     L = star[3] * (3.9*(10**26))
     r = 3 * 10**17
     F = L/(4 * math.pi * (r**2))
     k = star[8] + 2.5 * math.log10(F)
 
-    star.append(k)
+    star.append(round(k, 2))
 
     return star
 
+
 def readJson():
-    with open("stars.json", "r") as file:
+    with open("database.json", "r") as file:
         data = json.load(file)
     return data
 
+
 def writeJson(stars):
-    with open("stars.json", "w") as file:
+    with open("database.json", "w") as file:
         json.dump(stars, file, indent=5)
 
+
 def resetJson():
-    file = open("stars.json", "w")
+    file = open("database.json", "w")
     file.write("[]")
     file.close()
+
+
+def waveLength(T):
+    return 2860/T
+
 
 while True:
     table = createTable()
@@ -54,7 +71,9 @@ while True:
     print()
     print(table)
     print()
-    print("[A] - Add Estrela || [R] - Remover Estrela || [Q] - Sair || [RESET] - Resetar Estrelas")
+    print(f"Parâmetro: {sortField}")
+    print()
+    print("[A] - Add Estrela || [R] - Remover Estrela || [Q] - Sair || [RESET] - Resetar Estrelas || [O] - Ordenar Estrelas")
     option = str(input("O que deseja fazer: "))
     option = option.lower()
     option = option.split(" ")
@@ -64,6 +83,7 @@ while True:
         stars.append(newStar)
         writeJson(stars)
         table.add_row(newStar)
+        os.system("cls")
 
     if option[0] == "r":
         name = str(input("Nome da Estrela: ")).lower()
@@ -71,13 +91,22 @@ while True:
             if star[0].lower() == name:
                 del(stars[stars.index(star)])
                 writeJson(stars)
+        os.system("cls")
 
     elif option[0] == "reset":
         if "confirmar" == str(input("Escreva (confirmar)")).lower():
             resetJson()
+        os.system("cls")
 
-    elif option[0] == "c":
+    elif option[0] == "o":
+        print("Digite o número da característica")
+        print()
+        for name in field_names:
+            print(f"{field_names.index(name)} - {name}")
+        sortField = table.field_names[int(
+            input(f"Número do campo: 0 <-> {len(field_names) - 1}: "))]
         os.system("cls")
 
     elif option[0] == "q":
+        os.system("cls")
         break
